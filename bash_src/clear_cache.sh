4041c4f4d69f26c5acdb01257febc1d57856407f
HeavenHM <haseeb_heaven@yahoo.com>
2023-05-27 15:48:45 +0530

    Update clear_cache.sh

--- bash_src/clear_cache.sh
+++ bash_src/clear_cache.sh
@@ -5,12 +5,10 @@ if [ ! -d "codes" ]; then
   mkdir codes
 fi
 
-# removing all files from codes folder
-rm -rf codes/*
+# removing only all executables from codes.
+file -F: codes/* | grep "executable" | cut -d: -f1 | xargs rm
 
 # clearing response files and logs
-truncate -s 0 bardcoder.log
+#truncate -s 0 bardcoder.log
 truncate -s 0 response/content.md
 truncate -s 0 response/response.json
-
-
