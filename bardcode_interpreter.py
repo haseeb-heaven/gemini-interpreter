a913acc7f88039ba4fa007454c44a3c919149362
Haseeb <haseeb_heaven@yahoo.com>
2023-05-27 18:45:57 +0530

    Embed Graph output in app.

--- bardcode_interpreter.py
+++ bardcode_interpreter.py
@@ -17,21 +17,24 @@ from os import path
 import time
 import traceback
 from lib.bardcoder_lib import BardCoder
-from lib.extensions_map import get_streamlit_code_lang
 import subprocess
 from io import StringIO
-import requests
-import json
+import re
 from lib.sharegpt_api import sharegpt_get_url
+from PIL import Image
 
 # Initialize the bard coder
 bard_coder = BardCoder(enable_logs=True)
 
 # Function to measure the accuracy of the code
+
+
 def measure_accuracy(counter):
     accuracy = 1 / (counter + 1)
     accuracy_percentage = accuracy * 100
-    st.code(f"Output has been fixed {counter} times with accuracy {accuracy_percentage:.0f}%", language="python")
+    st.code(
+        f"Output has been fixed {counter} times with accuracy {accuracy_percentage:.0f}%", language="python")
+
 
 def show_content(content):
     # Open the file and read its contents
@@ -41,9 +44,11 @@ def show_content(content):
     # Display the Markdown text in the app
     st.markdown(markdown_text)
 
+
 # Initialize an empty string to hold the messages
 global log_container
 messages = ""
+
 
 def show_output(message):
     global messages
@@ -52,14 +57,16 @@ def show_output(message):
     log_container.code(st.session_state.messages, language="python")
 
 # method to execute the bard coder process
-def auto_bard_execute_process(prompt, code_file='code.txt', code_choices='code_choice', expected_output=None, exec_type='single',rate_limiter_delay=5):
+
+
+def auto_bard_execute_process(prompt, code_file='code.txt', code_choices='code_choice', expected_output=None, exec_type='single', rate_limiter_delay=5):
     try:
         # Additional prompt for class clarification.
-        prompt += "\n" + f"Note: The Name the class should be {code_file} if Java language is requested"
-        
+        # prompt += "\n" + f"Note: The Name the class should be {code_file} if Java language is requested"
+
         # Additional prompt for no input clarification.
         prompt += "\n" + "Dont ask the input from user.If input values are provided in code just use them. otherwise, you can hardcode the input values in code."
-        
+
         # Setting the prompt.
         bard_coder.set_prompt(prompt)
 
@@ -76,7 +83,8 @@ def auto_bard_execute_process(prompt, code_file='code.txt', code_choices='code_c
         code_output = bard_coder.execute_code(saved_file)
         if code_output and code_output != None and code_output.__len__() > 0:
             if 'error' in code_output.lower() or 'exception' in code_output.lower():
-                show_output(f"Error in executing code with exec_type {exec_type}")
+                show_output(
+                    f"Error in executing code with exec_type {exec_type}")
                 return code_output, saved_file, False
 
             # Check if expected output is in code output.
@@ -113,20 +121,25 @@ def auto_bard_execute_process(prompt, code_file='code.txt', code_choices='code_c
         show_output(str(e))
 
 # method to execute the bard coder process
-def auto_bard_setup_process(prompt, code_file='code.txt', code_choices='code_choice', expected_output=None, exec_type='single',rate_limiter_delay=5):
-    
+
+
+def auto_bard_setup_process(prompt, code_file='code.txt', code_choices='code_choice', expected_output=None, exec_type='single', rate_limiter_delay=5):
+
     # Append the codes directory to filename
-    code_file = path.join("codes",code_file)
-    test_cases_output = 0 # Test cases for output.
-        
+    code_file = path.join("codes", code_file)
+    test_cases_output = 0  # Test cases for output.
+
     # Start the bard coder process
-    code_choices_output, saved_file, status = auto_bard_execute_process(prompt, code_file, code_choices, expected_output, exec_type)
+    code_choices_output, saved_file, status = auto_bard_execute_process(
+        prompt, code_file, code_choices, expected_output, exec_type)
     code_output = None
-    
+
     if status:
-        show_output(f"Expected output found in file {saved_file}\nOutput: {code_choices_output}")
+        show_output(
+            f"Expected output found in file {saved_file}\nOutput: {code_choices_output}")
     else:
-        show_output(f"Expected output not found in file {saved_file}\nOutput: {code_choices_output}")
+        show_output(
+            f"Expected output not found in file {saved_file}\nOutput: {code_choices_output}")
         if code_choices_output:
             code_output = ''.join(code_choices_output)
         if code_output and code_output != None and code_output.__len__() > 0:
@@ -136,121 +149,168 @@ def auto_bard_setup_process(prompt, code_file='code.txt', code_choices='code_cho
                 code_output = "".join(code_output)
             else:
                 code_output = ""
-                
+
             if code_output:
                 while 'error' in code_output.lower() or 'exception' in code_output.lower():
-                    show_output(f"Error in executing code,Trying to fix the code with error")
+                    show_output(
+                        f"Error in executing code,Trying to fix the code with error")
 
                     # Re-prompt on error.
                     code = bard_coder.get_code()
                     prompt = f"I got error while running the code {code_output}.\nPlease fix the code ``\n`{code}\n``` \nand try again.\nHere is error {code_output}\n\n" + \
-                    "Note:The output should only be fixed code and nothing else. No explanation or anything else."
+                        "Note:The output should only be fixed code and nothing else. No explanation or anything else."
 
                     # Start the bard coder process again.
-                    code_output, saved_file, status = auto_bard_execute_process(prompt, code_file, code_choices, expected_output, exec_type)
+                    code_output, saved_file, status = auto_bard_execute_process(
+                        prompt, code_file, code_choices, expected_output, exec_type)
                     # Sleep for 5 seconds before re-prompting. Dont get Rate limited.
                     time.sleep(rate_limiter_delay)
                     test_cases_output += 1
-                
+
             show_output(f"Code has been fixed for error")
             st.code(code_output, language="python")
 
             # Check for expected output.
             if code_output and expected_output and code_output.__len__() > 0:
-                
+
                 # While expected output does not contain in code output.
                 while expected_output not in code_output:
-                    show_output(f"Expected output {expected_output} not found in code\nOutput: {code_output}")
+                    show_output(
+                        f"Expected output {expected_output} not found in code\nOutput: {code_output}")
 
                     # Re-prompt on expected output not found.
                     code = bard_coder.get_code()
                     prompt = f"I got output {code_output}.\nPlease fix the code ``\n`{code}\n```  \nand try again.\nHere is expected output: {code_output}\n\n" + \
-                    "Note:The output should only be fixed code and nothing else. No explanation or anything else."
+                        "Note:The output should only be fixed code and nothing else. No explanation or anything else."
 
                     # start the bard coder process again.
-                    code_output, saved_file, status = auto_bard_execute_process(prompt, code_file, code_choices, expected_output, exec_type)
-                    
-                    # Sleep for 5 seconds before re-prompting. Dont get Rate limited.
+                    code_output, saved_file, status = auto_bard_execute_process(
+                        prompt, code_file, code_choices, expected_output, exec_type)
+
+                    # Sleep for N seconds before re-prompting. Dont get Rate limited.
                     time.sleep(rate_limiter_delay)
                     test_cases_output += 1
-                    
+
                 show_output(f"Code has been fixed for expected output")
                 st.code(code_output, language="python")
             else:
-                show_output("Not checking for code expected output")            
+                show_output("Not checking for code expected output")
         else:
             show_output("Code output is empty for error")
-        
+
     # Print output and information.
-    measure_accuracy(test_cases_output)            
+    measure_accuracy(test_cases_output)
     content_file = "response/content.md"
     show_content(content_file)
-    return code_output
+    return code_output, saved_file, status
+
+
+def find_image_files(file_path):
+    # Create a regular expression for image files
+    image_regex = re.compile(r"\b\w+\.(png|jpg|jpeg|gif|bmp)", re.IGNORECASE)
+
+    # Open the code file
+    with open(file_path) as f:
+        # Read the lines
+        lines = f.readlines()
+        # Loop through the lines
+        for line in lines:
+            # Search for image files in the line
+            match = image_regex.search(line)
+            # If there is a match
+            if match:
+                # Get the image file name
+                image_file = match.group()
+                # Print the image file name
+                return image_file
+    return None
+
 
 if __name__ == "__main__":
     try:
         # Upload file data variables
-        upload_prompt_data,upload_data,uploaded_file = None,None,None
-        
+        upload_prompt_data, upload_data, uploaded_file = None, None, None
+
         # Initialize the session state variables
         if "code_output" not in st.session_state:
             st.session_state.code_output = ""
-        
-        if "messages" not in st.session_state:    
+
+        if "messages" not in st.session_state:
             st.session_state.messages = ""
-        
+
         # Set the page title and description
         st.title("AutoBard - Code Interpreter")
         prompt = st.text_area("Enter your prompt here:")
         bard_api_key = ""
-        
+
         # Setting options for the application
         with st.expander("Options"):
-            code_file = st.text_input("Filename for the generated code (without extension):", value="generated_code")
-            code_choices = st.text_input("Filename for code choices:", value="code_choices")
-            expected_output = st.text_input("Expected output (leave blank if none):")
-            exec_type = st.selectbox("Execution type:", ["single", "multiple"], index=0)
-            rate_limiter_delay = st.number_input("Rate limiter delay (in seconds):", value=5)
+            code_file = st.text_input(
+                "Filename for the generated code (without extension):", value="generated_code")
+            code_choices = st.text_input(
+                "Filename for code choices:", value="code_choices")
+            expected_output = st.text_input(
+                "Expected output (leave blank if none):")
+            exec_type = st.selectbox(
+                "Execution type:", ["single", "multiple"], index=0)
+            rate_limiter_delay = st.number_input(
+                "Rate limiter delay (in seconds):", value=5)
 
             # Adding the upload file option
             uploaded_file = st.file_uploader("Choose a file")
             if uploaded_file is not None:
-                
+
                 # To read file as bytes:
                 bytes_data = uploaded_file.getvalue()
                 # To convert to a string based IO:
                 stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
                 # To read file as string:
                 upload_data = stringio.read()
-                
+
                 # write the file to uploads directory
                 with open("uploads/" + uploaded_file.name, "w") as f:
                     f.write(upload_data)
-                
+
                 # Display a success message
                 st.success("File uploaded successfully.")
-            
-        # Setting the settings for the application    
+
+        # Setting the settings for the application
         with st.expander("Settings"):
             bard_api_key = st.text_input("Bard API key:", type="password")
             if bard_api_key:
                 bard_coder.set_api_key(bard_api_key)
-        
+
         # Seting application to run
         if st.button("Run"):
             # Clear the previous cache.
             st.code("Running the code generator", language="python")
             subprocess.call(['bash', 'bash_src/clear_cache.sh'])
-            
+
             # Append the uploaded file data to prompt
             if upload_data:
                 prompt += "\n" + f"Here is the file called {uploaded_file.name} at location {'uploads/' + uploaded_file.name} data.\n" + \
-                f"```\n{upload_data}\n```"
-                
+                    f"```\n{upload_data}\n```"
+
+            # If graph were requested.
+            if 'graph' in prompt.lower():
+                prompt += "\n" + "using Python use Matplotlib save the graph in file called 'graph.png'"
+            
+            if 'chart' in prompt.lower():
+                prompt += "\n" + "using Python use Plotly save the chart in file called 'chart.png'"
+
             # Run the auto bard setup process.
             log_container = st.empty()
-            st.session_state.code_output = auto_bard_setup_process(prompt, code_file, code_choices, expected_output, exec_type, rate_limiter_delay)
-            
+            st.session_state.code_output, saved_file, status = auto_bard_setup_process(
+                prompt, code_file, code_choices, expected_output, exec_type, rate_limiter_delay)
+
+            # Check if output is Graph,Chart request.
+            if 'graph' in prompt.lower() or 'chart' in prompt.lower():
+                image_file_graph = find_image_files(saved_file)
+                if image_file_graph:
+                    bard_coder.add_log(f"Graph image file is {image_file_graph} and code file is {saved_file}")
+                    image = Image.open(image_file_graph)
+                    st.image(image, caption='Graph Output')
+
         # Adding Share button
         if st.button("Share"):
             if st.session_state.code_output is None and st.session_state.messages is None:
@@ -258,20 +318,18 @@ if __name__ == "__main__":
             else:
                 gpt_data = prompt
                 human_data = ""
-                
+
                 if st.session_state.messages:
                     human_data = "Bard Logs: \n" + st.session_state.messages
                 if st.session_state.code_output:
                     human_data += "\nOutput:\n" + st.session_state.code_output
                 human_data += "\n\n[AutoBard-Coder: Repo](https://github.com/haseeb-heaven/AutoBard-Coder)"
-                
+
                 sharegpt_url = sharegpt_get_url(gpt_data, human_data)
-                #st.code(sharegpt_url, language="python")
+                # st.code(sharegpt_url, language="python")
                 st.code(f"ShareGPT Url: {sharegpt_url}", language="python")
 
-            
     except Exception as e:
         # show_outputf the stack trace
         stack_trace = traceback.format_exc()
         st.code(stack_trace, language="python")
-
